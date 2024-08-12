33622d00ea899c7c8367fd228ed11115b5749bd5
mcduck1 <daurenych@gmail.com>
2024-08-12 07:42:46 +0500

    second commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,2 +1,19 @@
-package hiber.model;public class Car {
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "model")
+    private String model;
+
+    @Column (name = "series")
+    private int series;
+
 }
